9ebcb628bfa0054b024fe6d27d024c122ee62722
rianaditro <rianaditro@gmail.com>
2024-02-02 21:06:04 +0700

    initiating flask web app

--- app.py
+++ app.py
@@ -1,16 +1,55 @@
-from flask import Flask
+from flask import Flask,flash,request,redirect,url_for
 from flask import render_template
-from db.convert import read_excel_to_html
+from werkzeug.utils import secure_filename
+from db.convert import read_excel_to_sqlite
+
+import sqlite3
+import os
+from pathlib import Path
+
+
+ALLOWED_EXTENSIONS = {"xls","xlsx"}
+BASE_DIR = Path(__file__).resolve().parent
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    html = read_excel_to_html("final_result.xlsx")
-    return render_template("/index.html",html=html)
+#app.config["DATABASE"] = "db/bookoutlet.sqlite"
+app.config["UPLOAD_FOLDER"] = os.path.join(BASE_DIR, "db/uploaded/")
 
-@app.route("/hallo")
-def hallo():
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".",1)[1].lower()
+
+@app.route("/", methods = ["GET","POST"])
+def upload_excel():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_path = app.config["UPLOAD_FOLDER"]
+            file.save(save_path+filename)
+            return redirect(url_for("/table"))
+    return render_template("/upload.html")
+
+@app.route("/table")
+def read_db():
+    """if request.method == "POST":
+        f = request.files['file']
+        sql_file = read_excel_to_sqlite(f)
+        cursor = sql_file.execute('SELECT * FROM bookTable LIMIT 100')
+        data = cursor.fetchall()
+        sql_file.close()"""
+    return render_template("/table.html")
+
+@app.route("/readPandas")
+def read_pandas():
     return "hallo"
 
 if __name__=="__main__":
--- db/convert.py
+++ db/convert.py
@@ -1,3 +1,7 @@
+from sqlalchemy import create_engine
+
+import sqlite3
+import json
 import pandas
 
 
@@ -6,3 +10,29 @@ def read_excel_to_html(filename):
     html = df.to_html()
     return html
     
+def read_excel_to_sql(filename):
+    df = pandas.read_excel(filename)
+    engine = create_engine("sqlite:///bookoutlet.db",echo=False)
+    df.to_sql("booksTable",con=engine)
+
+def read_excel_to_sqlite(filename):
+    df = pandas.read_excel(filename)
+    conn = sqlite3.connect("bookoutlet.sqlite")
+    df.to_sql("bookTable",conn)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db(filename):
+    db = sqlite3.connect(filename)
+    db.row_factory = sqlite3.Row
+    return db
+
+def excel_to_dict():
+    df = pandas.read_excel("final_result.xlsx")
+    datas = df.to_json(index=False)
+    products = json.loads(datas)
+    print(products)
+
+
+if __name__=="__main__":
+    read_excel_to_sqlite("final_result.xlsx")
