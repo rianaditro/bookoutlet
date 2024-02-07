8db16052d4cf99215c233d6e885f248ce3632df2
rianaditro <rianaditro@gmail.com>
2024-02-07 21:12:17 +0700

    navbar API

--- src/view_api.py
+++ src/view_api.py
@@ -6,30 +6,27 @@ import json,math
 
 api = Blueprint("api",__name__)
 
-@api.route("/api")
+@api.route("/api/book")
 def main():
+    page = request.args.get("page",1,type=int)
     per_page = 18
-    max_books = len(Book.query.all())
-    max_page = math.ceil(max_books/per_page)
-    template_retun = dict()
-    for page in range(1,max_page+1):
-        books = Book.query.paginate(page=page,per_page=per_page)
-        book_in_page = []
-        for book in books:
-            item = {
-                "title":book.title,
-                "author":book.author,
-                "price":book.price,
-                "binding":book.binding,
-                "isbn":book.isbn,
-                "publish_date":book.publish_date,
-                "publisher":book.publisher,
-                "language":book.language,
-                "page_count":book.page_count,
-                "dimension":book.dimension,
-                "image":book.image
-            }
-            book_in_page.append(item)
-        template_retun[page] = book_in_page
-    return jsonify(template_retun)
+    template_return = dict()
+    books = Book.query.paginate(page=page,per_page=per_page)
+    book_in_page = []
+    for book in books:
+        item = {
+            "title":book.title,
+            "author":book.author,
+            "price":book.price,
+            "binding":book.binding,
+            "isbn":book.isbn,
+            "publish_date":book.publish_date,
+            "publisher":book.publisher,
+            "language":book.language,
+            "page_count":book.page_count,
+            "dimension":book.dimension,
+            "image":book.image}
+        book_in_page.append(item)
+        template_return["result"] = book_in_page
+    return jsonify(template_return)
 
