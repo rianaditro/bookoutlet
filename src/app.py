347137805fe30c5773fa8d5ff88d6b4847d29d77
rianaditro <rianaditro@gmail.com>
2024-02-10 13:46:55 +0700

    config in a file

--- src/app.py
+++ src/app.py
@@ -1,24 +1,16 @@
 from flask import Flask
-from pathlib import Path
 
 from extensions import db,migrate,login_manager
 from views import view
 from view_api import api
-from view_login import login_user
-
-import os
-
-
-BASE_DIR = Path(__file__).resolve().parent
-UPLOAD_FOLDER = f"{BASE_DIR}/managed_db/uploaded"
+from user_view import user,login_manager
+from config import BaseConfig
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///old.sqlite"
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.config["DEBUG"] = True
+    app.config.from_object(BaseConfig)
 
     from models import Book,User
 
@@ -27,11 +19,12 @@ def create_app():
     login_manager.init_app(app=app)
 
     app.register_blueprint(api)
-    app.register_blueprint(login_user)
+    app.register_blueprint(user)
     app.register_blueprint(view)
 
     return app
 
 if __name__=="__main__":
+
     app = create_app()
     app.run()
--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
-import os
 from pathlib import Path
 
 class BaseConfig:
     BASE_DIR = Path(__file__).resolve().parent.parent
     SECRET_KEY = "TheSecretKey"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOAD_FOLDER  = os.path.join(BASE_DIR, 'media')
+    UPLOAD_FOLDER = f"{BASE_DIR}/managed_db/uploaded"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///old.sqlite"
+    DEBUG = True
