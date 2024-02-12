3703a5b7acfd18c6856c6b2806755f0f3d1e6747
rianaditro <rianaditro@gmail.com>
2024-02-12 09:12:40 +0700

    fix upload folder name

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ class BaseConfig:
     BASE_DIR = Path(__file__).resolve().parent.parent
     SECRET_KEY = "TheSecretKey"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOAD_FOLDER = f"{BASE_DIR}/managed_db/uploaded"
+    UPLOAD_FOLDER = f"{BASE_DIR}/manage_db/uploaded"
     SQLALCHEMY_DATABASE_URI = "sqlite:///old.sqlite"
     DEBUG = True
