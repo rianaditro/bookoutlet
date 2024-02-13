8fd988d8b856cb27a136e160d744da08d1aa677a
rianaditro <rianaditro@gmail.com>
2024-02-13 16:38:26 +0700

    sqlalchemy to mysql

--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ from pathlib import Path
 class BaseConfig:
     BASE_DIR = Path(__file__).resolve().parent.parent
     SECRET_KEY = "TheSecretKey"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOAD_FOLDER = f"{BASE_DIR}/manage_db/uploaded"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///old.sqlite"
     DEBUG = True
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = f"{BASE_DIR}/manage_db/uploaded"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:new-password@localhost:3306/book_db"
